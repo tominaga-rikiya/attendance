025cd08e58ce52bfbc4dc1760247b9eb0db43fc5
{tominaga rikiya} <{aodandesu@yahoo.co.jp}>
2025-05-19 18:22:59 +0900

    ロゴ修正

--- src/resources/views/components/admin_header.blade.php
+++ src/resources/views/components/admin_header.blade.php
@@ -1,6 +1,6 @@
 <header class="header">     
     <div class="header__logo">         
-        <a href="/"><img src="{{ asset('img/logo.png') }}" alt="ロゴ"></a>     
+        <img src="{{ asset('img/logo.png') }}" alt="ロゴ"></a>     
     </div>     
     @if(!in_array(Route::currentRouteName(), ['register', 'login', 'verification.notice']))         
         <nav class="header__nav">             
